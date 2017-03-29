08a5b190fc1458188d48acf6b338d188dd767edd
Shayne Torres <shaynetorres@Shaynes-MacBook-Pro.local>
2017-03-28 23:18:47 -0700

    add index files for middleware, routes, and app

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import http from "http";
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+
+import config from "./config";
+import routes from "./routes";
+
+let app = express();
+
+app.server = http.createServer(app);
+
+// JSON parsing
+
+app.use(bodyParser.json({
+    limit: config.bodyLimit
+}))
+
+// api routes v1
+app.use("/v1",routes);
+
+app.server.listen(config.port);
+console.log("Started listening on port ", app.server.address().port);
+
+export default app;
---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import config from "../config";
+import middleware from "../middleware";
+import initializeDB from "../db";
+
+let router = express();
+
+initializeDB(db=>{
+    router.use(middleware({config,db}));
+
+    // API routes
+})
