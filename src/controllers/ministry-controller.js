021adbe0544dfe2db2c20ca2efd345f5fe3ec651
Shayne Torres <shaynetorres@Shaynes-MacBook-Pro.local>
2017-03-28 23:39:31 -0700

    fix errors with routes

--- src/controllers/ministry-controller.js
+++ src/controllers/ministry-controller.js
@@ -18,13 +18,15 @@ export default({ config,db }) => {
         newMinistry.name = req.body.name;
         newMinistry.info = req.body.info;
         newMinistry.created_at = + new Date();
-        newMinistry.leaders = []
+        newMinistry.leaders.push(req.body.leader) ;
 
         newMinistry.save(err => {
             if (err){
                 res.send(err);
             }
-            res.json({message:"Your ministry was created successfully"})
+            res.json({message:"Your ministry was created successfully"});
         })
     })
+
+    return api;
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,16 @@ import config from "../config";
 import middleware from "../middleware";
 import initializeDB from "../db";
 
+import ministry from "../controllers/ministry-controller";
+
 let router = express();
 
 initializeDB(db=>{
     router.use(middleware({config,db}));
 
     // API routes
+    router.use("/ministry",ministry({config,db}));
+
 })
 
 export default router;
